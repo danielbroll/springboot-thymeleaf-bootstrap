9da1742313e782d76fc7a5508791d27307ace849
danielbroll <danielbroll@protonmail.com>
2022-08-15 13:39:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Dashboard
+
+Simple web application served with spring boot 2.7.2 using bootstrap 5.2.0
+
+See [bootstrap examples](https://getbootstrap.com/docs/5.2/examples/dashboard/)
+
+<img title="Dashboard" alt="Dashboard" src="/docs/dashboard.png">
---
+++ src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.frontbackend.thymeleaf.bootstrap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("version", "Bootstrap v5.2.0");
+        return "dashboard";
+    }
+}
